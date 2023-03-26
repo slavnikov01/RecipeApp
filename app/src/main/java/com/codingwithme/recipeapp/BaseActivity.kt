806527dc9c03ca8480686faf6e88285681870096
Slavnikov Bogdan <baizy228@mail.ru>
2023-03-26 15:12:10 +1000

    Initial commit

---
+++ app/src/main/java/com/codingwithme/recipeapp/BaseActivity.kt
@@ -0,0 +1,37 @@
+package com.codingwithme.recipeapp
+
+import android.app.job.JobInfo
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+open class BaseActivity : AppCompatActivity(),CoroutineScope {
+    private lateinit var job: Job
+    override val coroutineContext:CoroutineContext
+    get() = job +Dispatchers.Main
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        job = Job()
+
+        // Получение представления панели уведомлений
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+// Скрытие панели уведомлений
+        decorView.systemUiVisibility = uiOptions
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+}
